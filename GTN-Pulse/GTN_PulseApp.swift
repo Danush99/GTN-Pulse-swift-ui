343948b90ee726ca1e26be8a5b9f67a4b15fb83a
Danusha <danusha.99h@gmail.com>
2024-02-22 11:30:08 +0530

    Initial Commit

---
+++ GTN-Pulse/GTN_PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTN_PulseApp.swift
+//  GTN-Pulse
+//
+//  Created by Danusha Hewagama on 2024-02-22.
+//
+
+import SwiftUI
+
+@main
+struct GTN_PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
